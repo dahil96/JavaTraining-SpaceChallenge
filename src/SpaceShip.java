6163ff5c001462c78afb455be1c51c5eb1e7123b
Daniel-Laptop <dhilmersson@outlook.com>
2018-05-01 12:40:51 +0200

    First Setup

---
+++ src/SpaceShip.java
@@ -0,0 +1,7 @@
+public interface SpaceShip {
+    //Returns true for successful launch, false for failed launch
+    boolean launch();
+    //Returns true for successful landing, false for failed landing
+    boolean land();
+
+}
