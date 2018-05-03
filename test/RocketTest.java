8c694ba99b967d67eb2a8545e51cdeaf0e265575
Daniel-Laptop <dhilmersson@outlook.com>
2018-05-03 14:16:19 +0200

    Added skeleton RocketTest

---
+++ test/RocketTest.java
@@ -0,0 +1,14 @@
+public class RocketTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void canCarry() {
+    }
+}
